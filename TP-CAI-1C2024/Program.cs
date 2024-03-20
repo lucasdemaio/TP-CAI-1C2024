2e684440559c82ce6755fb63d92ef1c3f761f800
lucasdemaio <lucas.demaio25@hotmail.com>
2024-03-20 11:50:21 -0300

    primer commit

---
+++ TP-CAI-1C2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_CAI_1C2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
